b208ad0547544f7a5b4750b6b86588e35ed48aac
Gzim <gzimasanni@gmail.com>
2021-08-23 15:03:42 +0200

    Add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_124855) do
+ActiveRecord::Schema.define(version: 2021_08_23_125408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,15 @@ ActiveRecord::Schema.define(version: 2021_08_23_124855) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "score"
+    t.bigint "city_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["city_id"], name: "index_reviews_on_city_id"
+  end
+
+  add_foreign_key "reviews", "cities"
 end
