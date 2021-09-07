41577e7c313d1caa0accc7c1cc891c02c0380a7d
Gzim <gzimasanni@gmail.com>
2021-09-07 12:21:24 +0200

    Add heroku as host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  Rails.application.routes.default_url_options = { host: "https://cityreviewer.herokuapp.com/api/v1" }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
