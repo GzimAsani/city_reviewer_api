b78aa423b3904d7a69a17231b8440dd9ece5db09
Gzim <gzimasanni@gmail.com>
2021-09-11 16:06:36 +0200

    Add user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_125408) do
+ActiveRecord::Schema.define(version: 2021_09_11_140418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,18 @@ ActiveRecord::Schema.define(version: 2021_08_23_125408) do
     t.bigint "city_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["city_id"], name: "index_reviews_on_city_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reviews", "cities"
+  add_foreign_key "reviews", "users"
 end
